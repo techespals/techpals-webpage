52275789315733d18037df6fa93d2a0bbbd7c595
Tech Pals <tech.pals@gmail.com>
2026-05-21 09:18:07 +0530

    logo changed

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ const projects = [
           <div className="flex items-center gap-3">
 
             <img
-              src="/logo.jpg"
+              src="/logo.png"
               alt="TechPals Logo"
               className="w-12 h-12 object-contain"
             />
