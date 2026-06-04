99d417783d57abccac5a6ec8f26220907d90620f
Tech Pals <tech.pals@gmail.com>
2026-06-04 13:31:51 +0530

    changed url

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -26,7 +26,7 @@ function ContactForm() {
     try {
 
       await axios.post(
-        "https://techpals-backend-cvse.onrender.com",
+        "https://techpals-backend-cvse.onrender.com/api/inquiries",
         formData
       )
 
